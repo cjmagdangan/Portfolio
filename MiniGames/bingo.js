3086dbc198b1493e6ed9fac0f60da2d9cfb4306c
cjmagdangan <159784711+cjmagdangan@users.noreply.github.com>
2024-02-21 10:34:41 +0800

    Add files via upload

---
+++ MiniGames/bingo.js
@@ -0,0 +1,61 @@
+let haveIt = [];
+
+function roll() {
+  
+    let generate = Math.ceil(Math.random()*75);
+    //generate = Number(generate);
+        if(!haveIt.includes(generate)) {
+            var h=document.createElement("p");
+            var t=document.createTextNode(generate);
+            h.appendChild(t);
+            document.body.appendChild(h);
+            haveIt.push(generate);
+            if (generate<=15) {
+                document.getElementById("B").appendChild(h);
+                var msg = new SpeechSynthesisUtterance();
+                 msg.text = "B,"+generate;
+                  window.speechSynthesis.speak(msg);
+            } 
+            if (generate>15&&generate<=30) {
+                document.getElementById("I").appendChild(h);
+                var msg = new SpeechSynthesisUtterance();
+                 msg.text = "I,"+generate;
+                  window.speechSynthesis.speak(msg);
+            }
+            if (generate>30&&generate<=45) {
+                document.getElementById("N").appendChild(h);
+                var msg = new SpeechSynthesisUtterance();
+                 msg.text = "N,"+generate;
+                  window.speechSynthesis.speak(msg);
+            }
+            if (generate>45&&generate<=60) {
+                document.getElementById("G").appendChild(h);
+                var msg = new SpeechSynthesisUtterance();
+                 msg.text = "G,"+generate;
+                  window.speechSynthesis.speak(msg);
+            }
+            if (generate>60&&generate<=75) {
+                document.getElementById("O").appendChild(h);
+                var msg = new SpeechSynthesisUtterance();
+                 msg.text = "O,"+generate;
+                  window.speechSynthesis.speak(msg);
+            }
+            
+            else {
+                
+            }
+        }
+        else{
+            if (haveIt.length<75) {
+                return roll();
+            }
+            else{
+            alert("Wala na");
+            }
+        }
+}
+
+function restart() {
+    location.reload();
+}
+
